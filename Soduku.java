cc09579ed6d5322fb242c57db5f2969ba74a90f3
Cheonhong <clee95@dons.usfca.edu>
2020-09-04 16:43:51 +0900

    Fixed duplicates buf

--- Soduku.java
+++ Soduku.java
@@ -7,45 +7,6 @@ public class Soduku
 	public Soduku()
 	{
 		board = new int [9][9]; // created a 2d array to represent the board
-
-		for (int i = 0; i < 9; i++)
-		{
-			for (int j = 0; j < 9; j++)
-			{
-					board[i][j] = 0;
-			}
-		}
-
-		board[0][0] = 5;
-		board[0][1] = 3;
-		board[0][4] = 7;
-		board[1][0] = 6;
-		board[1][3] = 1;
-		board[1][4] = 9;
-		board[1][5] = 5;
-		board[2][1] = 9;
-		board[2][2] = 8;
-		board[2][7] = 6;
-		board[3][0] = 8;
-		board[3][4] = 6;
-		board[3][8] = 3;
-		board[4][0] = 4;
-		board[4][3] = 8;
-		board[4][5] = 3;
-		board[4][8] = 1;
-		board[5][0] = 7;
-		board[5][4] = 2;
-		board[5][8] = 6;
-		board[6][1] = 6;
-		board[6][6] = 2;
-		board[6][7] = 8;
-		board[7][3] = 4;
-		board[7][4] = 1;
-		board[7][5] = 9;
-		board[7][8] = 5;
-		board[8][4] = 8;
-		board[8][7] = 7;
-		board[8][8] = 9;
 	}
 
 	public boolean safe(int row, int col, int num)
@@ -161,7 +122,7 @@ public class Soduku
 			for(int c = 0; c < string.length(); c++){
 				char ch = string.charAt(c);
 				int num = Integer.parseInt(String.valueOf(ch));
-				if(s.safe(i, c, num) && num != 0)
+				if(!s.safe(i, c, num) && num != 0)
 				{
 					userSafe = false;
 					System.out.println("Error: You typed in a duplicate number"); // error for any duplicated numbers typed in by the user
